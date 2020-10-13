0041095b4ae77c5154e60000b6031e5294578feb
Ashvarya Sharma <sharma.ashvarya@gmail.com>
2020-10-12 21:30:45 -0500

    Build commit

---
+++ build/precache-manifest.0219e050cfcbcbf757700ce325c9071e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e511c906866844e88727605596b8e624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63aa17d3a0e4581beab7",
+    "url": "/static/css/main.c920e807.chunk.css"
+  },
+  {
+    "revision": "02c0526d652893b0e620",
+    "url": "/static/js/2.ffb68edb.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.ffb68edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63aa17d3a0e4581beab7",
+    "url": "/static/js/main.55f23133.chunk.js"
+  },
+  {
+    "revision": "59bde5ba28a8a5d646c1",
+    "url": "/static/js/runtime-main.f626648b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
